01d47f7479db3d7e5148c348a2072c65dc844218
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-01 12:58:45 +0200

    test different routes with params and else

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user/profile', function () {
+    return 'profile';
+})->name('user.profile');
+
+// group
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        return 'all';
+    });
+    Route::get('/user/{id}', function ($id) {
+        return $id;
+    });
+});
+
+Route::get('/city/{city?}', function ($city = 'Kyiv') {
+    return 'City ' . $city;
+})->whereAlpha('city');
+
+Route::get('/user/{id}', function ($id) {
+    return 'User ' . $id;
+});
+
+Route::get('/post/{slug}', function ($slug) {
+    return 'Post with slug ' . $slug;
+});
+
+Route::get('/posts/{date}', function ($date) {
+    return 'Date ' . $date;
+})->where('date', '^\d{2}-\d{2}-\d{4}$');
+
+Route::get('/{year}/{month}/{day}/', function ($year, $month, $day) {
+    return $year . '-' . $month . '-' . $day;
+})->where('year', '\d{2}')
+    ->where('month', '\d{2}')
+    ->where('day', '\d{4}');
+
+Route::get('/users/{order}', function ($order) {
+    return 'User ' . $order;
+})->whereIn('order', ['name', 'surname', 'age']);
