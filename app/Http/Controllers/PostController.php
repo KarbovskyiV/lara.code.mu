2ada34049b0f9cc6131175f07a83b6baa624c1ba
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-05 22:17:06 +0200

    test different possibility to work with blade

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,69 @@ class PostController extends Controller
     {
         return view('post.show', [
             'title' => 'Post title',
-            'text' => 'Post content',
+            'text' => 'Як обрати фреймворк для інтернет магазину',
+            'href' => 'https://uageneral.net/blog/it/yak-obraty-freymvork-dlya-internet-magazynu/',
+            'name' => 'Name',
+            'age' => '22',
+            'salary' => 10000,
+            'class' => 'text',
+            'input1' => 'My',
+            'input2' => 'Name',
+            'input3' => 'Is',
+            'color' => 'color:red',
+            'employee' => [
+                'name' => 'Employee name',
+                'age' => 24,
+                'salary' => 10000,
+            ],
+            'location' => [
+                'country' => 'Some country',
+                'city' => 'Some sity',
+            ],
+            'year' => '2022 ',
+            'month' => '12 ',
+            'day' => '02',
+            'str' => '<b>text</b>',
+            'isAuth' => 11,
+            'numbers' => [
+                2,
+                3,
+                5,
+                10,
+                0,
+                8,
+            ],
+            'data' => 44,
+            'array_str' => [
+                'str 1',
+                'str 2',
+                'str 3',
+                'str 4',
+            ],
+            'twoDarr' => [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+            ],
+            'employees' => [
+                [
+                    'name' => 'user1',
+                    'surname' => 'surname1',
+                    'salary' => 1000,
+                ],
+                [
+                    'name' => 'user2',
+                    'surname' => 'surname2',
+                    'salary' => 2000,
+                ],
+                [
+                    'name' => 'user3',
+                    'surname' => 'surname3',
+                    'salary' => 3000,
+                ],
+            ],
+            'users' => [],
+
         ]);
     }
 
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,5 +3,143 @@
         {{ $title }}-site.com
     </x-slot:title>
 
-    {{ $text }}
+    <p>Hello, {!! $str !!}</p>
+    current time: {{ date('d-m-Y') }}
+
+    <p>Name: {{ $employee['name'] }}</p>
+    <p>Age: {{ $employee['age'] }}</p>
+    <p>Salary: {{ $employee['salary'] }}</p>
+    <p>Count array:{{ count($employee) }}</p>
+
+    <ul>
+        @foreach($numbers as $number)
+            <li>{{ $number }} {{ 'в квадраті ' . $number * $number }}</li>
+        @endforeach
+    </ul>
+
+    Break
+    <p>
+        @foreach($numbers as $num)
+            {{ $num }}
+            @break($num === 0)
+        @endforeach
+    </p>
+
+    Continue
+    <p>
+        @foreach($numbers as $num)
+            @continue($num === 0)
+            {{ $num }}
+        @endforeach
+    </p>
+
+    @php
+        echo 'PHP';
+    @endphp
+
+    <p>
+        For loop
+        @for($i = 0; $i < 10; $i++)
+            {{ $i }}
+        @endfor
+    </p>
+
+    Loop var
+    <ul>
+        @foreach($array_str as $arr)
+            @if($loop->first)
+                <li class="first">{{ $arr }}</li>
+            @elseif($loop->last)
+                <li class="last">{{ $arr }}</li>
+            @endif
+            {{ $arr }}
+        @endforeach
+    </ul>
+
+    <p>Парні числа</p>
+    <ul>
+        @foreach($numbers as $number)
+            @if($number % 2 === 0)
+                <li>{{ $number }}</li>
+            @endif
+        @endforeach
+    </ul>
+
+    2 dimensional array
+    <ul>
+        @foreach($twoDarr as $array)
+            <li>
+                <ul>
+                    @foreach($array as $arr)
+                        <li>{{ $arr }}</li>
+                    @endforeach
+                </ul>
+            </li>
+        @endforeach
+    </ul>
+
+    multidimensional arrays
+    <ul>
+        @foreach($employees as $employee)
+            {{ $employee['name']}}
+            {{ $employee['surname']}}
+            {{ $employee['salary']}}
+        @endforeach
+    </ul>
+
+    Forelse
+    @forelse($users as $user)
+        <p>{{ $user }}</p>
+    @empty
+        <p>Users array empty</p>
+    @endforelse
+
+    @if(is_array($data))
+        @foreach($data as $num)
+            <ul>{{ $num }}</ul>
+        @endforeach
+    @elseif(is_numeric($data))
+        <p>{{ $data }}</p>
+    @endif
+
+    <ul>
+        @foreach($array_str as $key => $value)
+            <p>{{ $key + 1 . ' =>' }} {{ $value }}</p>
+        @endforeach
+    </ul>
+
+    {{--    @if(count($numbers) > 0)--}}
+    {{--        <p>{{ array_sum($numbers) }}</p>--}}
+    {{--    @elseif(count($numbers) === 0)--}}
+    {{--        <p>Empty</p>--}}
+    {{--    @endif--}}
+
+    <p>{{ $location['country'] ?? 'Ukraine' }}</p>
+    <p>{{ $location['city'] ?? 'Kyiv' }}</p>
+
+    <p>{{ $year ?? date('Y') }}</p>
+    <p>{{ $month ?? date('m') }}</p>
+    <p>{{ $day ?? date('d') }}</p>
+
+    <p class="{{ $class }}">Class</p>
+
+    <input value="{{ $input1 }}">
+    <input value="{{ $input2 }}">
+    <input value="{{ $input3 }}">
+
+    <p style="{{ $color }}">Paragraph</p>
+    <a href="{{ $href }}" target="_blank">{{ $text }}</a>
+
+    @unless($isAuth > 18)
+        <p>Too young</p>
+    @endunless
+
+    @if($isAuth > 18)
+        <p>Text for old</p>
+    @elseif($isAuth < 18)
+        <p> Text for young</p>
+    @else
+        <p>Text for 18 year old</p>
+    @endif
+
 </x-layout>
