569ac8b82c22a0dd2648a6c606c68a0ee8be5e3f
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-06 11:46:51 +0200

    work with blade usage different possibillity

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,103 @@ class UserController extends Controller
         return view('user.show', [
             'title' => 'User title',
             'text' => 'User content',
+            'links' => [
+                [
+                    'text' => 'text1',
+                    'href' => 'href1',
+                ],
+                [
+                    'text' => 'text2',
+                    'href' => 'href2',
+                ],
+                [
+                    'text' => 'text3',
+                    'href' => 'href3',
+                ],
+            ],
+            'employees' => [
+                [
+                    'name' => 'user1',
+                    'surname' => 'surname1',
+                    'salary' => 1000,
+                ],
+                [
+                    'name' => 'user2',
+                    'surname' => 'surname2',
+                    'salary' => 2000,
+                ],
+                [
+                    'name' => 'user3',
+                    'surname' => 'surname3',
+                    'salary' => 3000,
+                ],
+                [
+                    'name' => 'user4',
+                    'surname' => 'surname4',
+                    'salary' => 4000,
+                ],
+                [
+                    'name' => 'user5',
+                    'surname' => 'surname5',
+                    'salary' => 5000,
+                ],
+            ],
+            'users' => [
+                [
+                    'name' => 'user1',
+                    'surname' => 'surname1',
+                    'banned' => true,
+                ],
+                [
+                    'name' => 'user2',
+                    'surname' => 'surname2',
+                    'banned' => false,
+                ],
+                [
+                    'name' => 'user3',
+                    'surname' => 'surname3',
+                    'banned' => true,
+                ],
+                [
+                    'name' => 'user4',
+                    'surname' => 'surname4',
+                    'banned' => false,
+                ],
+                [
+                    'name' => 'user5',
+                    'surname' => 'surname5',
+                    'banned' => false,
+                ],
+            ],
+            'array_str' => [
+                'str1',
+                'str2',
+                'str3',
+                'str4',
+            ],
+            'days' => [
+                1,
+                2,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                15,
+                16,
+                18,
+                20,
+                22,
+                23,
+                26,
+                28,
+                29,
+                30
+            ],
+            'today' => 6,
+
         ]);
     }
 
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -3,5 +3,75 @@
         {{ $title }}-site.com
     </x-slot:title>
 
-    {{ $text }}
+    Links
+    <ul>
+        @foreach($links as $link)
+            <li><a href="{{ $link['href'] }}">{{ $link['text'] }}</a></li>
+        @endforeach
+    </ul>
+
+    Table employees
+    <table>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Salary</th>
+        </tr>
+        @foreach($employees as $employee)
+            <tr>
+                @if($employee['salary'] > 2000)
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $employee['name'] }}</td>
+                    <td>{{ $employee['surname'] }}</td>
+                    <td>{{ $employee['salary'] }}</td>
+                @endif
+            </tr>
+        @endforeach
+    </table>
+    <br>
+
+    Table users
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Status</th>
+        </tr>
+        @foreach($users as $user)
+            <tr>
+                <td>{{ $user['name'] }}</td>
+                <td>{{ $user['surname'] }}</td>
+                @if($user['banned'] === true)
+                    <td style="color: red">{{ 'banned' }}</td>
+                @elseif($user['banned'] === false)
+                    <td style="color: green">{{ 'active' }}</td>
+                @endif
+            </tr>
+        @endforeach
+    </table>
+
+    <p>Input</p>
+    @foreach($array_str as $str)
+        <input type="text" value="{{ $str }}">
+    @endforeach
+
+    <p>Select</p>
+    <select name="str" id="str">
+        @foreach($array_str as $str)
+            <option value="{{ $str }}">{{ $str }}</option>
+        @endforeach
+    </select>
+
+    <p>Days</p>
+    <ul>
+        @foreach($days as $day)
+            @if($day === $today)
+                <li class="active">{{ $day }}</li>
+                @continue
+            @endif
+            <li>{{ $day }}</li>
+        @endforeach
+    </ul>
+
 </x-layout>
