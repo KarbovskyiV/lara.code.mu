634b621399c984f07ea8de7db6c4f121758f4c15
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-07 10:51:50 +0200

    testing use migration

---
+++ database/migrations/2023_03_06_105830_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('email', 100)->comment('Only @gmail');
+            $table->unsignedInteger('age')->nullable();
+            $table->string('surname', 100);
+            $table->unsignedInteger('salary')->default(0);
+            $table->date('birthday');
+            $table->dateTime('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
