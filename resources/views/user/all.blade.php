5c23d1a11591a81f2d83ad733790d92c29777efa
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-10 18:28:58 +0200

    output users use model eloqent

--- resources/views/user/all.blade.php
+++ resources/views/user/all.blade.php
@@ -3,5 +3,22 @@
         {{ 'All-users' }}-site.com
     </x-slot:title>
 
-    {{ dump($names) }}
+    <table>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>Salary</th>
+        </tr>
+        @foreach($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->age }}</td>
+                <td>{{ $user->salary }}</td>
+            </tr>
+        @endforeach
+    </table>
 </x-layout>
