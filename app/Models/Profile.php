bd5c9df0db1eedfdf7e03b4ec021e882a01a3cba
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-13 13:16:35 +0200

    create models and add relationshsp with them

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class City extends Model
+class Profile extends Model
 {
     use HasFactory;
 
-    public function country(): BelongsTo
+    public function user(): BelongsTo
     {
-        return $this->belongsTo(Country::class);
+        return $this->belongsTo(User::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
