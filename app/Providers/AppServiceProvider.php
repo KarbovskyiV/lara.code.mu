ccee19cc01d6e8ca1e408c14149c06225ea75c0d
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-21 20:54:26 +0200

    add global access variable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        View::share('date', date('Y'));
+
+        View::composer('user*', function ($view) {
+            $view->with('balance', 12345);
+        });
+
         Paginator::useBootstrapFive();
     }
 }
