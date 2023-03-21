660b59b763a25c02d67e17e43667da27bb7fc0f8
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-21 20:50:48 +0200

    change view name

--- routes/main.php
+++ routes/main.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\User\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
+Route::view('/', 'home.index')->name('home');
 
 Route::get('/test', TestController::class)->name('test')->middleware('token:secret');
 
