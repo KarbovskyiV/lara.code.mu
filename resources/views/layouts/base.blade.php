7a75170bb4adb963dd3ebc8a34af42593956184a
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-23 11:40:18 +0200

    create layouts view

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,12 +5,24 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css">
     <title>@yield('page.title', config('app.name'))</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css">
+    @stack('css')
+    <style>
+        .container {
+            max-width: 720px;
+        }
+
+        .required:after {
+            content: '*';
+            color: red;
+        }
+    </style>
 </head>
 <body>
 
-<div class="d-flex flex-column justify-content-between min-vh-100 text-center">
+<div class="d-flex flex-column justify-content-between min-vh-100">
     @include('includes.header')
 
     <main class="flex-grow-1 py-3">
@@ -21,6 +33,6 @@
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/js/bootstrap.min.js"></script>
-
+@stack('js')
 </body>
 </html>
