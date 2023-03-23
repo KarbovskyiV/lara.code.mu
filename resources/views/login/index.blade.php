b37aa95190110d99d42d07974aa7a259e824f0fc
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-23 11:38:24 +0200

    create login view

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,9 +1,7 @@
-@extends('layouts.base')
+@extends('layouts.auth')
 
 @section('page.title', 'Login')
 
-@section('content')
-    <h1>
-        Login
-    </h1>
+@section('auth.content')
+    <x-login.card/>
 @endsection
