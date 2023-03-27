74f5f25fac2231901213da230a3c1b56178a8fd5
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-27 17:57:14 +0300

    add checked

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -39,7 +39,7 @@
                 </x-form-item>
 
                 <x-form-item>
-                    <x-checkbox name="agreement">
+                    <x-checkbox name="agreement" :checked="!! old('agreement')">
                         {{ __('I agree to the processing of personal data') }}
                     </x-checkbox>
                 </x-form-item>
