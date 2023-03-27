98aa3f4233df0815e336f0af637bfbd0f2380e6f
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-27 13:03:44 +0300

    add search logic for posts in blog

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,20 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $search = $request->input('search');
+        $category_id = $request->input('category_id');
+
+//        dd($search, $category_id);
+
         $post = (object)[
             'id' => 123,
             'title' => 'Lorem ipsum dolor sit amet.',
-            'content' => 'Lorem ipsum <strong>dolor</strong> sit amet, consectetur adipisicing elit. Eligendi, eos!'
+            'content' => 'Lorem ipsum <strong>dolor</strong> sit amet, consectetur adipisicing elit. Eligendi, eos!',
+            'category_id' => 1
         ];
 
         $posts = array_fill(0, 10, $post);
 
-        return view('blog.index', compact('posts'));
+        $posts = array_filter($posts, function ($post) use ($search, $category_id) {
+            if ($search && !str_contains(strtolower($post->title), strtolower($search))) {
+                return false;
+            }
+
+            if ($category_id && $post->category_id != $category_id) {
+                return false;
+            }
+
+
+            return true;
+        });
+
+        $categories = [
+            null => __('All category'),
+            1 => __('First category'),
+            2 => __('Second category'),
+        ];
+
+        return view('blog.index', compact('posts', 'categories'));
     }
 
     public function show($post)
--- resources/views/components/header.blade.php
+++
@@ -1,8 +0,0 @@
-<header>
-    header
-    <x-alert/>
-    <logo>
-        https://www.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg
-    </logo>
-    <x-contact/>
-</header>
