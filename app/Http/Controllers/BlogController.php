23861be582718f32eea8eebefeac5f0c2e9b2fb8
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-03-31 15:08:37 +0300

    add validation and logic of ordering posts

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,32 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     public function index(Request $request)
     {
-        $categories = [
-            null => __('All category'),
-            1 => __('First category'),
-            2 => __('Second category'),
-        ];
+        $validated = $request->validate([
+            'search' => ['nullable', 'string', 'max:50'],
+            'from_date' => ['nullable', 'string', 'date'],
+            'to_date' => ['nullable', 'string', 'date', 'after:from_date'],
+            'tag' => ['nullable', 'string', 'max:20'],
+        ]);
 
-        $posts = Post::query()
-            ->latest('published_at')
+        $query = Post::query()
+            ->where('published', true)
+            ->whereNotNull('published_at');
+
+        if ($search = $validated['search'] ?? null) {
+            $query->where('title', 'like', "%{$search}%");
+        }
+
+        if ($fromDate = $validated['from_date'] ?? null) {
+            $query->where('published_at', '>=', new Carbon($fromDate));
+        }
+
+        if ($toDate = $validated['to_date'] ?? null) {
+            $query->where('published_at', '<=', new Carbon($toDate));
+        }
+
+        if ($tag = $validated['tag'] ?? null) {
+            $query->whereJsonContains('tags', $tag);
+        }
+
+        $posts = $query->latest('published_at')
             ->paginate(12, ['id', 'title', 'published_at']);
 
-        return view('blog.index', compact('posts', 'categories'));
+        return view('blog.index', compact('posts'));
     }
 
     public function show($post)
     {
-        $post = (object)[
-            'id' => 123,
-            'title' => 'Lorem ipsum dolor sit amet.',
-            'content' => 'Lorem ipsum <strong>dolor</strong> sit amet, consectetur adipisicing elit. Eligendi, eos!'
-        ];
+        $post = Post::query()->findOrFail($post);
 
         return view('blog.show', compact('post'));
     }
