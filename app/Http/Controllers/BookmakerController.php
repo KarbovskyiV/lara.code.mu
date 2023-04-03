14f10e2f704682da5db93b8a4e95d716064d3854
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-04-03 13:08:30 +0300

    create controller, add index method

---
+++ app/Http/Controllers/BookmakerController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookmakerController extends Controller
+{
+    public function index()
+    {
+        return view('bookmaker.index');
+    }
+
+    public function create()
+    {
+    }
+
+    public function store()
+    {
+    }
+
+    public function show()
+    {
+    }
+
+    public function edit()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
+}
